603c1485ca4e0cd6cfff4de3ee9e05c3122d802e
bai <invincibleyao@hotmail.com>
2018-06-02 18:03:27 +0800

    construct cluster

--- cnn_seg/cluster.py
+++ cnn_seg/cluster.py
@@ -1,31 +1,29 @@
 import numpy as np
 import os
 
-
 obj_type={
-    "META_UNKNOWN": 1,
-    "META_SMALLMOT": 2,
-    "META_BIGMOT": 3,
-    "META_NONMOT": 4,
-    "META_PEDESTRIAN": 5,
-    "MAX_META_TYPE":6
+    1 : "META_UNKNOWN",
+    2 : "META_SMALLMOT",
+    3 : "META_BIGMOT",
+    4 : "META_NONMOT",
+    5 : "META_PEDESTRIAN",
+    6 : "MAX_META_TYPE"
 }
 
 class Object(object):
     def __init__(self):
-        self.grid = np.zeros([640, 640])
+        self.grid = None
         self.cloud = None
         self.score = 0
         self.height = 0
         self.meta_type = None
         self.meta_type_probs = None
 
-
-class UnionFind(object):
+class Node(object):
     def __init__(self):
         self.center_node = None
         self.parent = None
-        self.node_rank =None
+        self.node_rank = None
         self.traversed = None
         self.is_center = False
         self.is_object = False
@@ -33,10 +31,13 @@ class UnionFind(object):
         self.obstacle_id = 0
 
 
-
-
 class Cluster(object):
     def __init__(self, row, col, range):
+        """
+        :param row: row of grid in clustering range
+        :param col: colum of grid in clustering range
+        :param range: before and after distance(meters) in 3D cloud points
+        """
         self.rows_ = row
         self.cols_ = col
         self.grids_ = row*col
@@ -49,7 +50,24 @@ class Cluster(object):
         self.id_img_ = None
         self.bin = None
 
-    def Cluster2Grid(self, category_pt, instance_pt, bin, valid_indices):pass
+    def IsValidRowCol(self, row, col): return self.IsValidRow(row) and self.IsValidCol(col)
+    def IsValidRow(self, row): return row >= 0 and row < self.rows_
+    def IsValidCol(self, col): return col >= 0 and col < self.cols_
+    def RowCol2Grid(self, row, col): return row * self.cols_ + col
+    def GetTypeText(self, obj_type):
+        if obj_type == "VEHICLE": return "car"
+        elif obj_type == "PEDESTRIAN": return "pedestrian"
+        elif obj_type == "BICYCLE": return "bicycle"
+        else: return "unknown"
+
+    def Cluster2Grid(self, category_pt, instance_pt, bin, valid_indices):
+        self.bin = np.fromfile(bin, np.float32).reshape([-1, 4])
+        total_point_num = self.bin.shape[0]
+        assert len(valid_indices) == total_point_num, \
+            "size of 3d cloud points is not equal to valid indices"
+
+
+
 
     def Filter(self, confidence_pt, height_pt):pass
 
@@ -57,15 +75,8 @@ class Cluster(object):
 
     def GetObjects(self, confidence_thresh, height_thresh, min_pt_num):pass
 
-    def IsValidRowCol(self):pass
-
-    def IsValidRow(self, row): return row >= 0 and row < self.rows_
-
-    def IsValidCol(self, col): return col >= 0 and col < self.cols_
-
-    def RowCol2Grid(self, row, col): return row * self.cols_ + col
 
 
 
-
-
+if __name__ == "__main__":
+    print(obj_type[6])
